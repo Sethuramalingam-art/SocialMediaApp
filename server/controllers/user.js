b7903ad9a10ec976e5f66a7bdb95d9993358dc92
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2023-05-30 23:50:51 +0530

    Update user.js - Added add remove friend logics

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -28,6 +28,7 @@ export const getUserFriends = async (request, response) => {
         return { _id, firstName, lastName, occupation, location, picturePath };
       }
     );
+    response.status(200).json(formattedFriends);
   } catch (error) {
     response.status(404).json({ message: error.message });
   }
@@ -37,6 +38,29 @@ export const getUserFriends = async (request, response) => {
 
 export const addRemoveFriend = async (request, response) => {
   try {
+    const { id, friendId } = request.params;
+    const user = await User.findById(id);
+    const friend = await User.findById(friendId);
+    if (user.friends.includes(friendId)) {
+      user.friends = user.friends.filter((id) => id !== friendId);
+      friend.friends = friend.friends.filter((id) => id !== id);
+    } else {
+      user.friends.push(friendId);
+      friend.freinds.push(id);
+    }
+    await user.save();
+    await friend.save();
+
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+
+    const formattedFriends = friends.map(
+      (_id, firstName, lastName, occupation, location, picturePath) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+    response.status(200).json(formattedFriends);
   } catch (error) {
     response.status(404).json({ message: error.message });
   }
