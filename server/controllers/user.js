6e38492a1de9c19def2a32ad97e4e0c0b358e137
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2023-05-30 23:15:21 +0530

    Update user.js

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,1 +1,43 @@
+import User from "../models/User";
 
+// READ USER
+
+export const getUser = async (request, response) => {
+  try {
+    const { id } = request.params;
+    const user = await User.findById(id);
+    response.status(200).json(user);
+  } catch (error) {
+    response.status(404).json({ message: error.message });
+  }
+};
+
+// READ USER FRIENDS
+
+export const getUserFriends = async (request, response) => {
+  try {
+    const { id } = request.params;
+    const user = await User.findById(id);
+
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+
+    const formattedFriends = friends.map(
+      (_id, firstName, lastName, occupation, location, picturePath) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+  } catch (error) {
+    response.status(404).json({ message: error.message });
+  }
+};
+
+// UPDATE FRIEND DETAILS
+
+export const addRemoveFriend = async (request, response) => {
+  try {
+  } catch (error) {
+    response.status(404).json({ message: error.message });
+  }
+};
