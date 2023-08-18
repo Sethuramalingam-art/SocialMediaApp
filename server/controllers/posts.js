7f03d7ac9d541a53c906be76b5f056a2c08fa8a1
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2023-08-18 16:49:52 +0530

    Create posts.js

---
+++ server/controllers/posts.js
@@ -0,0 +1,27 @@
+import Post from "../models/Post";
+import User from "../models/User";
+
+// CREATE
+
+export const createPost = async (req, res) => {
+  try {
+    const { userId, description, picturePath } = req.body;
+    const user = await User.findById(userId);
+    const newPost = new Post({
+      userId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      location: user.location,
+      description,
+      userPicturePath: user.userPicturePath,
+      picturePath,
+      likes: {},
+      comments: [],
+    });
+    await newPost.save();
+    const post = await Post.find();
+    res.status(201).json(post);
+  } catch (err) {
+    res.status().json({ message: err.message });
+  }
+};
