af7202ba2329cc082bf964d18a72209c9d91eee3
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2023-09-06 08:39:07 +0530

    Update App.js - includes routing config

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 
-import './App.css';
-
+import LoginPage from "scenes/loginPage";
+import HomePage from "scenes/homePage";
+import ProfilePage from "scenes/profilePage";
+import Navbar from "scenes/navbar";
 function App() {
   return (
     <div className="app">
-   
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
