d374907e9f5af961cd1eac9e53dea331a297c8b9
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2023-10-02 23:31:41 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,26 +9,32 @@ import {
 import LoginPage from "scenes/loginPage";
 import HomePage from "scenes/homePage";
 import ProfilePage from "scenes/profilePage";
-import { useMemo } from 'react';
-import { useSelector } from 'react-redux'
-import { CssBaseline , ThemeProvider } from '@mui/material';
-import { createTheme } from '@mui/material/styles';
-import { themeSettings } from './theme'
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
-
-  const mode = useSelector((state)=> state.mode);
-  const theme = useMemo(()=> createTheme(themeSettings(mode)), [mode]);
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
   return (
     <div className="app">
       <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
+          </Routes>
         </ThemeProvider>
       </BrowserRouter>
     </div>
@@ -36,5 +42,3 @@ function App() {
 }
 
 export default App;
-
-
